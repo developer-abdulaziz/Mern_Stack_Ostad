a24c2aa2a04354dede75f8c84793b3c7b2d09648
Md Abdul Aziz <abdulazizwebdeveloper1996@gmail.com>
2023-08-29 07:48:22 +0600

    file update

---
+++ index.js
@@ -0,0 +1,34 @@
+/*======================= How Request Response Model Works in node js ===========================*/
+/*
+var http=require('http');
+var server=http.createServer(function (req,res) {
+ 
+  if(req.url=="/"){
+     res.writeHead(200,{'Content-Type':'text/html'});
+     res.write('This is my home page');
+     res.end();
+  }
+  else if(req.url=="/Contact"){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('This is my home contact page');
+    res.end();
+
+  }
+  else if(req.url=="/About"){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('This is my home about');
+    res.end();  
+}
+
+  else if(req.url=="/Terms"){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('This is my term page');
+    res.end();
+  }
+
+});
+server.listen(7000);
+console.log("Server Run Success");
+*/
+
+
